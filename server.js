ad02384a1e79099a37eb234ed8130c5c043c667f
Majzik "Mini Pablo" Bence <majben686@hengersor.hu>
2025-02-17 14:14:25 +0100

    server beallitasa
    mysql server kapcsolatanak letrehozasa ellenorzo GET megirasa

--- server.js
+++ server.js
@@ -6,6 +6,21 @@ const bodyParser = require("body-parser");
 app.use(cors())
 app.use(bodyParser.json());
 
+const db = mysql.createConnection(
+    {
+        user: "root",
+        host: "localhost",
+        port: 3307,
+        password: "",
+        database: "teliolimpia",
+    }
+    //a server futasanak ellenorzese
+);
+
+app.get("/", (req, res) => {
+    res.send("Szia üdvözzöllek! Ügyes vagy fut a backend!");
+})
+
 app.listen(3000, () => {
     console.log("A server a 3000-es porton fut!");
 });
